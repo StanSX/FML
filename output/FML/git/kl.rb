b4b6579a4a46fb0163d4e7a1a910ae75c62c0113
garymawdsley <garymawdsley@googlemail.com>
2009-09-08 21:44:16 +0100

    updated params

--- output/FML/git/kl.rb
+++ output/FML/git/kl.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'kerrylondon'
 
-dataTweak = {'estturnover'=>1000000,'plwageroll0'=>80000,'plwageroll3'=>20000,'contplant'=>500000}
+dataTweak = {'estturnover'=>100000,'plwageroll0'=>80000,'plwageroll3'=>20000}
 
 #---------------------  username        password    product   saveQuote?    data to overrode the default submission
 result = Kerrylondon.makeQuote("Tony Lorriman","liability1","CSP",false,Kerrylondon.keyfacts(dataTweak))
